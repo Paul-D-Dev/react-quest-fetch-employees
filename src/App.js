99dc3ea7659f3a694099ee6421787a008cd08d03
Paul DAO <dao.van.paul@gmail.com>
2020-03-25 16:59:16 +0100

    remove preload employee replace by a conditionnal rendering

--- src/App.js
+++ src/App.js
@@ -3,31 +3,11 @@ import DisplayEmployee from './components/DisplayEmployee';
 import axios from 'axios';
 import LoadingSpinner from './components/LoadingSpinner';
 
-const sampleEmployee = {
-  gender: 'male',
-  name: {
-    first: 'Charlie',
-    last: 'Thompson',
-  },
-  location: {
-    street: {
-      number: 761,
-      name: 'Tay Street',
-    },
-    city: 'Timaru',
-    postcode: 76111,
-  },
-  email: 'charlie.thompson@example.com',
-  picture: {
-    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
-  },
-};
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      employee: sampleEmployee
+      employee: null
     }
 
     this.getEmployee = this.getEmployee.bind(this);
@@ -58,10 +38,11 @@ class App extends React.Component {
 
   render() {
     const {loading} = this.state; 
-
+    const initEmployee = this.state.employee ? <DisplayEmployee employee={this.state.employee} /> : <p>No data yet</p>
+    
     return (
       <div>
-        {loading ? <LoadingSpinner/> : <DisplayEmployee employee={this.state.employee}/> }
+        {loading ? <LoadingSpinner/> : initEmployee }
         < button type="button" onClick={this.getEmployee}>Get employee</button>
       </div>
     );
