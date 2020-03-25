56d3e8fd78e4eebc600756474adda9c1a6a6d99f
Paul DAO <dao.van.paul@gmail.com>
2020-03-25 15:08:41 +0100

    transform App component to a class and display a employee with state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DisplayEmployee from './components/DisplayEmployee';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = sampleEmployee;
+  }
+  render() {
+
+    return (
+      <div>
+      <DisplayEmployee employee={sampleEmployee}/>
     </div>
-  );
+    );
+  } 
 }
 
 export default App;
